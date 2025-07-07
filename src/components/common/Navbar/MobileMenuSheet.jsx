d5896831b9fbf6b3afe3a770b959d608398ba4fa
ajijul-islam-dev <ajijulislamdev@gmail.com>
2025-07-07 21:36:57 +0600

    navbar mobile menu section sidebar created

--- src/components/common/Navbar/MobileMenuSheet.jsx
+++ src/components/common/Navbar/MobileMenuSheet.jsx
@@ -12,11 +12,13 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarProvider,
+  SidebarSeparator,
 } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { AlignJustify } from "lucide-react";
@@ -28,28 +30,33 @@ const MobileMenuSheet = () => {
     <Sheet>
       <SheetTrigger>
         <div>
-          <div className="outline outline-primary p-2 rounded-md text-primary">
-            <AlignJustify />
+          <div className="outline outline-primary p-2 rounded-md  text-primary">
+            <AlignJustify size="20px" />
           </div>
         </div>
       </SheetTrigger>
-      <SheetContent side="left">
+      <SheetContent className="overflow-hidden" side="left">
         <SheetHeader>
           <SheetTitle>Are you absolutely sure?</SheetTitle>
         </SheetHeader>
+        <SidebarSeparator />
         <SidebarProvider>
-          <SidebarMenu>
-            {items.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <Link href={item.url}>
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
+          <SidebarContent>
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarContent>
         </SidebarProvider>
       </SheetContent>
     </Sheet>
