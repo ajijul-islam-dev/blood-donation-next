805dbe3c127e3dec0d9ae6539e67fd0615383c42
= <=>
2025-07-10 19:18:00 +0600

    ui impooved

--- src/components/common/InspireSection/InspireCard.jsx
+++ src/components/common/InspireSection/InspireCard.jsx
@@ -3,7 +3,7 @@ import { ShinyButton } from "@/components/magicui/shiny-button";
 import Image from "next/image";
 import React, { useState } from "react";
 
-const InspireCard = () => {
+const InspireCard = ({ inspire }) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <div
@@ -18,9 +18,9 @@ const InspireCard = () => {
         <div className=" overflow-hidden">
           <Image
             className={`${
-              isHovered ? "scale-110" : ""
-            } transition-all overflow-hidden duration-500 w-full object-cover`}
-            src="/assets/images/Inspiration/donor.png"
+              isHovered ? "scale-125" : ""
+            } transition-all overflow-hidden duration-500 w-full object-cover h-80`}
+            src={inspire.image}
             width={500}
             height={500}
             alt="Inspire"
@@ -28,8 +28,8 @@ const InspireCard = () => {
           <Image
             className={`${
               isHovered ? "bg-primary" : "bg-black"
-            }  w-24 transition-colors duration-500 h-24 p-4 px-6 absolute left-0 right-0 mx-auto -bottom-5`}
-            src="/assets/images/Inspiration/icons/first-aid.png"
+            }  w-20 transition-colors duration-500 h-20 p-4 px-6 absolute left-0 right-0 mx-auto -bottom-5`}
+            src={inspire.icon}
             width={100}
             height={100}
             alt="Inspire"
@@ -37,11 +37,8 @@ const InspireCard = () => {
         </div>
       </div>
       <div className="text-center bg-white p-5 space-y-5 pt-8">
-        <h2 className="text-2xl">Become a Donor</h2>
-        <p className="text-base">
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give pleasure
-        </p>
+        <h2 className="text-2xl">{inspire.title}</h2>
+        <p className="text-base">{inspire.description}</p>
       </div>
       <div className="w-full">
         <ShinyButton
@@ -49,7 +46,7 @@ const InspireCard = () => {
             isHovered ? "bg-primary" : "bg-black"
           } transition-colors duration-500 text-white w-full rounded-none p-5`}
         >
-          <span className="text-white">Shiny Button</span>
+          <span className="text-white">Read More</span>
         </ShinyButton>
       </div>
     </div>
--- src/components/common/InspireSection/InspireSection.jsx
+++ src/components/common/InspireSection/InspireSection.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import InspireCard from "./InspireCard";
+import { Description } from "@radix-ui/react-dialog";
 
 const InspireSection = () => {
   return (
     <div className="bg-[#f5f5f5] py-20">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-16">
-        <InspireCard />
-        <InspireCard />
-        <InspireCard />
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 ">
+        {inspires?.map((inspire) => (
+          <InspireCard inspire={inspire} key={inspire.id} />
+        ))}
       </div>
     </div>
   );
 };
 
 export default InspireSection;
+
+const inspires = [
+  {
+    id: 1,
+    image: "/assets/images/Inspiration/donor.png",
+    icon: "/assets/images/Inspiration/icons/first-aid.png",
+    title: "Become a Donor",
+    description:
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give pleasure",
+  },
+  {
+    id: 2,
+    image: "/assets/images/Inspiration/blood.png",
+    icon: "/assets/images/Inspiration/icons/tube.png",
+    title: "Why Give Blood?",
+    description:
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give pleasure",
+  },
+  {
+    id: 3,
+    image: "/assets/images/Inspiration/donations.png",
+    icon: "/assets/images/Inspiration/icons/heart.png",
+    title: "How Donations Helps",
+    description:
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give pleasure",
+  },
+];
